78083787a2358ef2b38cec0f49d307eadf38cb00
Ashley McNamara <amcnamara9179@gmail.com>
2024-12-02 16:26:50 -0500

    rover interaction

--- Planet_Parv_Game/Assets/Scripts/RoverFollowsPlayer.cs
+++ Planet_Parv_Game/Assets/Scripts/RoverFollowsPlayer.cs
@@ -1,28 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoverFollowsPlayer : MonoBehaviour
 {
     public Transform player;
+    public GameObject RoverCanvas;
+    public Text StoryText;
 
     private float speed = 5.0f;
     private float stopDistance = 2.5f;
     private float rotationSpeed = 5.0f;
+    private bool roverShouldFollowPlayer = false;
+    private float interactionRange = 10.0f;
+
+    void Start()
+    {
+        RoverCanvas.SetActive(false);
+    }
 
     void Update()
     {
-        float distToPlayer = Vector3.Distance(transform.position, player.position);
-
-        // If the rover is close to player, stop moving
-        if (distToPlayer > stopDistance)
+        if (!roverShouldFollowPlayer)
         {
-            // move rover toward player
-            transform.position += transform.forward * speed * Time.deltaTime;
+            // check if player is looking at rover
+            Ray ray = new Ray(player.position, player.forward);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 1.0f, out hit, interactionRange) && hit.collider.CompareTag("Rover"))
+            {
+                RoverCanvas.SetActive(true);
+                // if player presses e, make the rover follow them
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    roverShouldFollowPlayer = true;
+                }
+            }
+            else
+            {
+                RoverCanvas.SetActive(false);
+            }
+        }
 
-            // rotate rover toward player
-            Quaternion targetRotation = Quaternion.LookRotation(player.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        if (roverShouldFollowPlayer)
+        {
+            RoverCanvas.SetActive(false);
+            float distToPlayer = Vector3.Distance(transform.position, player.position);
+
+            // If the rover is close to player, stop moving
+            if (distToPlayer > stopDistance)
+            {
+                // move rover toward player
+                transform.position += transform.forward * speed * Time.deltaTime;
+
+                // rotate rover toward player
+                Quaternion targetRotation = Quaternion.LookRotation(player.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
         }
     }
 }
