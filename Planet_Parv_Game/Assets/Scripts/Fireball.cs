a85926002141a64821ca198ebaa3f0b9a2b8c098
Ashley McNamara <amcnamara9179@gmail.com>
2024-12-16 14:29:42 -0500

    added a mode to the UFO where it predicts where the player will be and shoots there when the player is close to the sample; made the player respawns when getting hit; and made the UFO stop attacking if the player is far away

--- Planet_Parv_Game/Assets/Scripts/Fireball.cs
+++ Planet_Parv_Game/Assets/Scripts/Fireball.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    private GameObject astronaut;
+    private Vector3 respawnPoint = new Vector3(62.32672f, 19.30546f, 6.414683f);
+    private CharacterController controller;
+
     private void Start()
     {
+        astronaut = GameObject.FindGameObjectWithTag("Player");
+        controller = astronaut.GetComponent<CharacterController>();
         Destroy(gameObject, 5.0f);
     }
     void OnCollisionEnter(Collision collision)
@@ -14,6 +20,9 @@ public class Fireball : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player hit!");
+            controller.enabled = false;
+            astronaut.transform.position = respawnPoint;
+            controller.enabled = true;
         }
         // fireball gets destroyed after hitting anything
         Destroy(gameObject);
